b248e5bba3266dbf6d8eddecda757ec81df76f6e
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-19 08:33:21 +1000

    Fix Betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,17 +16,17 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  int leftHeight = 0;
-  int rightHeight = 0;
+	int leftHeight = 0;
+	int rightHeight = 0;
   
-  if (tree == NULL || binary_tree_is_leaf(tree))
-    return (0);
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
   
-  leftHeight = binary_tree_height(tree->left);
-  rightHeight = binary_tree_height(tree->right);
+	leftHeight = binary_tree_height(tree->left);
+	rightHeight = binary_tree_height(tree->right);
 
-  if (leftHeight > rightHeight)
-    return (leftHeight + 1);
+	if (leftHeight > rightHeight)
+		return (leftHeight + 1);
 
-  return (rightHeight + 1);
+	return (rightHeight + 1);
 }
