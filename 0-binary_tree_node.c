8425303dcd2310a00a00c321d9cff11d9864c9a8
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-18 21:02:11 +1000

    Fix betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
   binary_tree_t *new_node;
 
   new_node = malloc(sizeof(*new_node));
