d016e02ba530717a983a7f18b2f2b25262553531
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-19 08:32:17 +1000

    Binary tree height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left != NULL)
+		return (0);
+
+	if (node->right != NULL)
+		return (0);
+
+	return (1);
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+  int leftHeight = 0;
+  int rightHeight = 0;
+  
+  if (tree == NULL || binary_tree_is_leaf(tree))
+    return (0);
+  
+  leftHeight = binary_tree_height(tree->left);
+  rightHeight = binary_tree_height(tree->right);
+
+  if (leftHeight > rightHeight)
+    return (leftHeight + 1);
+
+  return (rightHeight + 1);
+}
