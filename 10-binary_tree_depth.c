b89fd4e5596a6e3818da30919d037efb889e705d
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-19 08:48:17 +1000

    Fix Betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,8 +13,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  if (tree == NULL || binary_tree_is_root(tree))
-    return (0);
+	if (tree == NULL || binary_tree_is_root(tree))
+		return (0);
 
-  return 1 + binary_tree_depth(tree->parent);
+	return (1 + binary_tree_depth(tree->parent));
 }
