41304c2fffd64bb99755c4d7de5aaf9d89f87e3e
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-19 09:52:36 +1000

    Bugfix: No need to handle case when both left and right pointers are

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -5,9 +5,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-
 	if (tree->left != NULL && tree->right != NULL)
 		return (2);
 
