8b98b9dcc73b2784b839f4677b0393a12ff87e32
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-19 09:41:43 +1000

    Indentation

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,14 +2,14 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-  size_t leftLenght = 0;
-  size_t rightLenght = 0;
+	size_t leftLenght = 0;
+	size_t rightLenght = 0;
 
-  if (tree == NULL)
-    return (0);
+	if (tree == NULL)
+		return (0);
 
-  leftLenght = binary_tree_size(tree->left);
-  rightLenght = binary_tree_size(tree->right);
+	leftLenght = binary_tree_size(tree->left);
+	rightLenght = binary_tree_size(tree->right);
 
-  return (leftLenght + rightLenght + 1);
+	return (leftLenght + rightLenght + 1);
 }
