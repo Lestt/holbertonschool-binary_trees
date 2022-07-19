6362e656c99ca30facec7d9968217c70b5d46b54
Jonathan Clus <jonathan.clus@holbertonschool.com>
2022-07-19 10:47:36 +1000

    Bugfix: wrong algo implementation

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,11 +2,17 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	int numberOfLeavesInLeft = 0;
+	int numberOfLeavesInRight = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
-		return (2);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	return (1);
+	numberOfLeavesInLeft = binary_tree_leaves(tree->left);
+	numberOfLeavesInRight = binary_tree_leaves(tree->right);
+
+	return (numberOfLeavesInLeft + numberOfLeavesInRight);
 }
